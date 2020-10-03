196bef568b936f89bc492de7f217b2c279687b57
Paul Tobin <tobster111@gmail.com>
2020-10-03 20:50:05 +0100

    Using an old style loop

--- StephanGrider/exercises/reversestring/index.js
+++ StephanGrider/exercises/reversestring/index.js
@@ -6,8 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {
-  return str.split('').reverse().join('');
-}
+
+
 
 module.exports = reverse;
+
+
+
+// using reverse
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+// Old style loop
+// function reverse(str) {
+//   let reversed = '';
+
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversed += str[i];
+//   }
+//   return reversed;
+// }
