103c4add23b55f1101089ba877c7ea50ef8f1b20
Paul Tobin <tobster111@gmail.com>
2020-10-09 19:20:13 +0100

    Added more tests

--- StephanGrider/exercises/anagrams/test.js
+++ StephanGrider/exercises/anagrams/test.js
@@ -12,6 +12,18 @@ test('"Whoa! Hi!" is an anagram of "Hi! Whoa!"', () => {
   expect(anagrams('Whoa! Hi!', 'Hi! Whoa!')).toBeTruthy();
 });
 
+test('"rail safety" is an anagram of "fairy tales" ', () => {
+  expect(anagrams('rail safety', 'fairy tales')).toBeTruthy();
+});
+
+test('"RAIL! SAFETY!" is an anagram of "fairy tales" ', () => {
+  expect(anagrams('rail safety', 'fairy tales')).toBeTruthy();
+});
+
+test('"Hi there" is an anagram of "Bye there"', () => {
+  expect(anagrams('Hi there', 'Bye there')).toBeFalsy();
+});
+
 test('"One One" is not an anagram of "Two two two"', () => {
   expect(anagrams('One One', 'Two two two')).toBeFalsy();
 });
