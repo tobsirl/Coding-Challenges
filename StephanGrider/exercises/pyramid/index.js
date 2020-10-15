3e2e0fd115571f19c3c082e8a8ade0b3476f812c
Paul Tobin <tobster111@gmail.com>
2020-10-15 17:58:14 +0100

    Solution to the pyramid problem

--- StephanGrider/exercises/pyramid/index.js
+++ StephanGrider/exercises/pyramid/index.js
@@ -14,6 +14,20 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const midpoint = Math.floor((2 * n - 1) / 2);
+
+  for (let row = 0; row < n; row++) {
+    let level = '';
+    for (let col = 0; col < n * 2 - 1; col++) {
+      if (midpoint - row <= col && midpoint + row >= col) {
+        level += '#';
+      } else {
+        level += ' ';
+      }
+    }
+    console.log(level);
+  }
+}
 
 module.exports = pyramid;
