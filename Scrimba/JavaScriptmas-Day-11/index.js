a6d6586f83d8741b1921829600f7d7158d8d2f57
Paul Tobin <tobster111@gmail.com>
2020-12-11 11:11:38 +0000

    Solution complete, added second test

--- Scrimba/JavaScriptmas-Day-11/index.js
+++ Scrimba/JavaScriptmas-Day-11/index.js
@@ -1,6 +1,11 @@
 function avoidObstacles(nums) {
+  const sortNums = nums.sort();
 
-  
+  for (let i = 0; i < sortNums.length; i++) {
+    if (sortNums.every((el) => el % i > 0)) {
+      return i;
+    }
+  }
 }
 
 module.exports = avoidObstacles;
--- Scrimba/JavaScriptmas-Day-11/test.js
+++ Scrimba/JavaScriptmas-Day-11/test.js
@@ -11,4 +11,15 @@ describe('avoidObstacles', () => {
     // assert
     expect(result).toBe(4);
   });
+
+  test('returns minimal number of jumps in between numbers 2', () => {
+    // arrange
+    const nums = [5, 2, 8, 7, 11, 1, 13];
+
+    // act
+    const result = avoidObstacles(nums);
+
+    // assert
+    expect(result).toBe(3);
+  });
 });
