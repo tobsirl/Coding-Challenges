8138e92c4cb2eef44e1b2c723f2d1d66ef76798b
Paul Tobin <tobster111@gmail.com>
2020-12-13 11:52:23 +0000

    Additional test added, with solution

--- Scrimba/JavaScriptmas-Day-13/index.js
+++ Scrimba/JavaScriptmas-Day-13/index.js
@@ -1,5 +1,5 @@
 function extractEachKth(nums, index) {
-  
+  return nums.filter((number) => number % index !== 0);
 }
 
 module.exports = extractEachKth;
--- Scrimba/JavaScriptmas-Day-13/test.js
+++ Scrimba/JavaScriptmas-Day-13/test.js
@@ -1,7 +1,7 @@
 const extractEachKth = require('./index');
 
 describe('extractEachKth', () => {
-  test('returns largest positive integer possible for digit count ', () => {
+  test('returns largest positive integer possible for digit count 1', () => {
     // arrange
     const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     const index = 3;
@@ -23,5 +23,17 @@ describe('extractEachKth', () => {
 
     // assert
     expect(result).toEqual([1, 3, 5, 7, 9]);
+  });
+
+  test('returns largest positive integer possible for digit count 3', () => {
+    // arrange
+    const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    const index = 4;
+
+    // act
+    const result = extractEachKth(nums, index);
+
+    // assert
+    expect(result).toEqual([1, 2, 3, 5, 6, 7, 9, 10, 11]);
   });
 });
