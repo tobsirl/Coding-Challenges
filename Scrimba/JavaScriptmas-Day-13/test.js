09107e21b3b05445c3ab65ac2e093b46bb9b369c
Paul Tobin <tobster111@gmail.com>
2020-12-13 11:49:16 +0000

    Added another test

--- Scrimba/JavaScriptmas-Day-13/test.js
+++ Scrimba/JavaScriptmas-Day-13/test.js
@@ -12,4 +12,16 @@ describe('extractEachKth', () => {
     // assert
     expect(result).toEqual([1, 2, 4, 5, 7, 8, 10]);
   });
+
+  test('returns largest positive integer possible for digit count 2', () => {
+    // arrange
+    const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const index = 2;
+
+    // act
+    const result = extractEachKth(nums, index);
+
+    // assert
+    expect(result).toEqual([1, 3, 5, 7, 9]);
+  });
 });
