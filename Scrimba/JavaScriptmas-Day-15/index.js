dc875e2db4f7b4b7601037f1e6085b05a5386d98
Paul Tobin <tobster111@gmail.com>
2020-12-15 09:37:29 +0000

    Solution to day 15

--- Scrimba/JavaScriptmas-Day-15/index.js
+++ Scrimba/JavaScriptmas-Day-15/index.js
@@ -1,1 +1,29 @@
 // javascript
+const prev = document.querySelector('.previous');
+const next = document.querySelector('.next');
+const gallery = document.querySelector('.gallery');
+const images = document.querySelector('.gallery img');
+
+let counter = 0;
+const gallerySize = 220;
+
+prev.addEventListener('click', () => {
+  if (counter <= 0) return;
+  next.style.opacity = '1';
+  gallery.style.transition = 'transform .7s ease-in-out';
+  counter--;
+
+  if (counter <= 0) prev.style.opacity = '0.3';
+
+  gallery.style.transform = 'translateX(' + -gallerySize * counter + 'px)';
+});
+
+next.addEventListener('click', () => {
+  if (counter >= images.length - 1) return;
+  prev.style.opacity = '1';
+  gallery.style.transition = 'transform .7s ease-in-out';
+  counter++;
+  
+  if (counter >= images.length - 1) next.style.opacity = '0.3';
+  gallery.style.transform = 'translateX(' + -gallerySize * counter + 'px)';
+});
