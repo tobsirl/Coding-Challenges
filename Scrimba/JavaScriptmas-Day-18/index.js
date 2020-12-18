162fa9382b40bb4857e383036cdb8edfd57479ef
Paul Tobin <tobster111@gmail.com>
2020-12-18 11:44:48 +0000

    Solution

--- Scrimba/JavaScriptmas-Day-18/index.js
+++ Scrimba/JavaScriptmas-Day-18/index.js
@@ -1,3 +1,9 @@
-function arrayPreviousLess(nums) {}
+function arrayPreviousLess(nums) {
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    nums[i - 1] < nums[i] ? result.push(nums[i - 1]) : result.push(-1);
+  }
+  return result;
+}
 
 module.exports = arrayPreviousLess;
