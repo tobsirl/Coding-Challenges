adf160abe48d8d5c05a056e8c98b6919ee09e1cd
Paul Tobin <tobster111@gmail.com>
2020-12-19 10:03:24 +0000

    Added another test and solution

--- Scrimba/JavaScriptmas-Day-19/index.js
+++ Scrimba/JavaScriptmas-Day-19/index.js
@@ -1,3 +1,17 @@
-function alphabetSubsequence(str) {}
+function alphabetSubsequence(str) {
+  if (typeof str !== 'string') return false;
+
+  const hasDuplicates = new Set(str).size;
+
+  if (str.length > hasDuplicates) return false;
+
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str.charAt(i) >= str.charAt(i + 1)) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 module.exports = alphabetSubsequence;
--- Scrimba/JavaScriptmas-Day-19/test.js
+++ Scrimba/JavaScriptmas-Day-19/test.js
@@ -1,15 +1,29 @@
-const alphabetSubsequence = require("./index");
+const alphabetSubsequence = require('./index');
 
 describe('alphabetSubsequence()', () => {
+  test('returns false if it is not a string', () => {
+    // arrange
+    const str = 84928734;
+
+    // act
+    const result = alphabetSubsequence(str);
+
+    // log
+    console.log('result 1: ', result);
+
+    // assert
+    expect(result).toBe(false);
+  });
+
   test('returns false when it has duplicate letters', () => {
     // arrange
     const str = 'effg';
 
     // act
-    const result = alphabetSubsequencealphabetSubsequence(str);
+    const result = alphabetSubsequence(str);
 
     // log
-    console.log('result 1: ', result);
+    console.log('result 2: ', result);
 
     // assert
     expect(result).toBe(false);
@@ -23,13 +37,13 @@ describe('alphabetSubsequence()', () => {
     const result = alphabetSubsequence(str);
 
     // log
-    console.log('result 2: ', result);
+    console.log('result 3: ', result);
 
     // assert
     expect(result).toBe(false);
   });
 
-  test('returns true whenno duplicates and is ascending a - z order ', () => {
+  test('returns true when no duplicates and is ascending a - z order ', () => {
     // arrange
     const str = 'ace';
 
@@ -37,7 +51,7 @@ describe('alphabetSubsequence()', () => {
     const result = alphabetSubsequence(str);
 
     // log
-    console.log('result 3: ', result);
+    console.log('result 4: ', result);
 
     // assert
     expect(result).toBe(true);
