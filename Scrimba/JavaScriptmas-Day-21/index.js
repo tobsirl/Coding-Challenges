b2d71515fd4bdf1aceb97d31ad1ef0f6fa8d3172
Paul Tobin <tobster111@gmail.com>
2020-12-21 12:23:20 +0000

    Solution

--- Scrimba/JavaScriptmas-Day-21/index.js
+++ Scrimba/JavaScriptmas-Day-21/index.js
@@ -1,3 +1,17 @@
-function sumOfTwo(nums1, nums2, value) {}
+function sumOfTwo(nums1, nums2, value) {
+  let restObj = {};
+  nums1.forEach((el) => {
+    let rest = value - el;
+    restObj[rest] = rest;
+  });
+
+  for (let number of nums2) {
+    if (restObj.hasOwnProperty(number)) {
+      return true;
+    }
+  }
+  
+  return false;
+}
 
 module.exports = sumOfTwo;
