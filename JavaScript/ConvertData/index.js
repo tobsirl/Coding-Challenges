d2ec2689e68b3c4ed0edf45442583fb53efdef41
Paul Tobin <tobster111@gmail.com>
2021-03-03 14:54:36 +0000

    Random object stuff

---
+++ JavaScript/ConvertData/index.js
@@ -0,0 +1,96 @@
+const obj = {
+  start_minute: 90,
+  end_minute: 210,
+  days: [0, 1, 2],
+};
+
+
+
+const arr = [...obj.days];
+
+arr.map((el) => {
+  if (el === 0) {
+  }
+}); //?
+
+// obj.days.push(obj.start_minute)
+// obj.days.push(obj.end_minute)
+
+obj.days; //?
+
+const days = 'days';
+const test = obj[days].map((d) => {
+  let key;
+  if (d === 0) {
+    key = 'Sunday';
+  }
+  if (d === 1) {
+    key = 'Monday';
+  }
+  if (d === 2) {
+    key = 'Tuesday';
+  }
+
+  if (d === 3) {
+    key = 'Wednesday';
+  }
+
+  return {
+    [key]: [],
+  };
+});
+
+const key = 'Sunday';
+
+test; //?
+
+Object.values(test); //?
+
+const covertedObj = Object.entries(obj).map((data) => {
+  const x = 'days';
+  const value = data[x];
+  return value;
+});
+
+covertedObj; //?
+
+function convert(obj) {
+  const arr = [...obj.days];
+
+  const newObj = arr.map((day) => {
+    let key;
+    if (day === 0) {
+      key = 'Sunday';
+    }
+
+    if (day === 1) {
+      key = 'Monday';
+    }
+
+    if (day === 2) {
+      key = 'Tuesday';
+    }
+
+    return {
+      [key]: [],
+    };
+  });
+  return newObj;
+}
+
+const [sunday, monday, tuesday] = convert(obj); //?
+
+const t = sunday;
+
+t.Sunday.push(obj.start_minute);
+
+sunday; //?
+
+// convert to
+const converted = {
+  Sunday: [{ start_minute: 90, end_minute: 210 }],
+  Monday: [{ start_minute: 90, end_minute: 210 }],
+  Tuesday: [{ start_minute: 90, end_minute: 210 }],
+};
+
+
