d78166c79ee1eb3618996ae4bf99efaf9f87d5ea
Paul Tobin <tobster111@gmail.com>
2021-03-19 22:56:23 +0000

    Updating an object

--- JavaScript/ConvertData/index-times.js
+++ JavaScript/ConvertData/index-times.js
@@ -1,6 +1,29 @@
-import { dayIndexedTimes } from './data';
+import { dayIndexedTimes, data } from './data';
 
 dayIndexedTimes; //?
 
-dayIndexedTimes[0]; //?
 dayIndexedTimes[1]; //?
+
+data;
+
+const key = 'start_minute';
+const value = 30;
+
+function updateObj(key, value) {
+  // data[0].start_minute = key;
+  const { firstObj } = data[0];
+  firstObj;
+  data[key] = value;
+  key; //?
+  value;
+}
+
+updateObj(key, value);
+
+data;
+
+const objIdx = data.findIndex((obj) => obj.start_minute === 240);
+
+const updatedObj = { ...data[objIdx], end_minute: 300 };
+
+updatedObj
