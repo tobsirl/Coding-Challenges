fbfa20e0d3b2b3c6a1fdbeb91c93bf785ef2a478
Paul Tobin <tobster111@gmail.com>
2021-03-20 18:49:57 +0000

    Object basics

--- JavaScript/Objects/index.js
+++ JavaScript/Objects/index.js
@@ -2,3 +2,17 @@ import { jonas } from './data';
 
 jonas.lastName; //?
 jonas['lastName']; //?
+
+const nameKey = 'Name';
+jonas['first' + nameKey]; //?
+jonas[`last${nameKey}`]; //?
+
+jonas.location = 'Ireland';
+jonas['twitter'] = '@jonas';
+
+jonas; //?
+
+// challenge
+const result = `${jonas['firstName']} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`;
+
+result; //?
