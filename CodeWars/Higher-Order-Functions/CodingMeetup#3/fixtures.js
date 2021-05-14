8180d9830b7d474d074a22a30760f0e0c148bb8d
Paul Tobin <tobster111@gmail.com>
2021-05-14 15:15:05 +0100

    Added index and fixtures

---
+++ CodeWars/Higher-Order-Functions/CodingMeetup#3/fixtures.js
@@ -0,0 +1,45 @@
+export const list1 = [
+  {
+    firstName: 'Sofia',
+    lastName: 'I.',
+    country: 'Argentina',
+    continent: 'Americas',
+    age: 35,
+    language: 'Java',
+  },
+  {
+    firstName: 'Lukas',
+    lastName: 'X.',
+    country: 'Croatia',
+    continent: 'Europe',
+    age: 35,
+    language: 'Python',
+  },
+  {
+    firstName: 'Madison',
+    lastName: 'U.',
+    country: 'United States',
+    continent: 'Americas',
+    age: 32,
+    language: 'Ruby',
+  },
+];
+
+export const list2 = [
+  {
+    firstName: 'Sofia',
+    lastName: 'I.',
+    country: 'Argentina',
+    continent: 'Americas',
+    age: 35,
+    language: 'Java',
+  },
+  {
+    firstName: 'Lukas',
+    lastName: 'X.',
+    country: 'Croatia',
+    continent: 'Europe',
+    age: 35,
+    language: 'Python',
+  },
+];
---
+++ CodeWars/Higher-Order-Functions/CodingMeetup#3/index.js
@@ -0,0 +1,15 @@
+import { list1, list2 } from './fixtures';
+/*
+You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return:
+
+true if at least one Ruby developer has signed up; or
+false if there will be no Ruby developers.
+*/
+
+function isRubyComing(list) {
+  return list.some((dev) => dev.language === 'Ruby');
+}
+
+isRubyComing(list1);
