544c0df6b2d05248571ca5b888b0998acf0b0b8d
Paul Tobin <tobster111@gmail.com>
2021-05-15 16:25:11 +0100

    Setup

---
+++ CodeWars/Higher-Order-Functions/CodingMeetup#4/index.fixtures.js
@@ -0,0 +1,50 @@
+const list1 = [
+  {
+    firstName: 'Mark',
+    lastName: 'G.',
+    country: 'Scotland',
+    continent: 'Europe',
+    age: 22,
+    language: 'JavaScript',
+  },
+  {
+    firstName: 'Victoria',
+    lastName: 'T.',
+    country: 'Puerto Rico',
+    continent: 'Americas',
+    age: 30,
+    language: 'Python',
+  },
+  {
+    firstName: 'Emma',
+    lastName: 'B.',
+    country: 'Norway',
+    continent: 'Europe',
+    age: 19,
+    language: 'Clojure',
+  },
+];
+
+const list2 = [
+  {
+    firstName: 'Kseniya',
+    lastName: 'T.',
+    country: 'Belarus',
+    continent: 'Europe',
+    age: 29,
+    language: 'JavaScript',
+  },
+  {
+    firstName: 'Amar',
+    lastName: 'V.',
+    country: 'Bosnia and Herzegovina',
+    continent: 'Europe',
+    age: 32,
+    language: 'Ruby',
+  },
+];
+
+module.exports = {
+  list1,
+  list2,
+};
