a6080391787fe1d763a267b255544a723ab17419
Paul Tobin <tobster111@gmail.com>
2021-05-16 16:09:36 +0100

    Better version

--- CodeWars/Higher-Order-Functions/CodingMeetup#5/index.js
+++ CodeWars/Higher-Order-Functions/CodingMeetup#5/index.js
@@ -33,43 +33,49 @@ const list1 = [
   },
 ];
 
+// function countLanguages(list) {
+//   const languageCount = list.map((dev) => {
+//     let c = 0;
+//     let javaScript = 0;
+//     let ruby = 0;
+//     if (dev.language === 'C') {
+//       c++;
+//     }
+//     if (dev.language === 'JavaScript') {
+//       javaScript++;
+//     }
+//     if (dev.language === 'Ruby') {
+//       ruby++;
+//     }
+
+//     return {
+//       C: c,
+//       JavaScript: javaScript,
+//       Ruby: ruby,
+//     };
+//   });
+
+//   const countC = Object.values(languageCount)
+//     .map((el) => el.C)
+//     .reduce((accum, curr) => accum + curr, 0);
+//   const countJavaScript = Object.values(languageCount)
+//     .map((el) => el.JavaScript)
+//     .reduce((accum, curr) => accum + curr, 0);
+//   const countRuby = Object.values(languageCount)
+//     .map((el) => el.Ruby)
+//     .reduce((accum, curr) => accum + curr, 0);
+
+//   return {
+//     C: countC,
+//     JavaScript: countJavaScript,
+//     Ruby: countRuby,
+//   };
+// }
+
 function countLanguages(list) {
-  const languageCount = list.map((dev) => {
-    let c = 0;
-    let javaScript = 0;
-    let ruby = 0;
-    if (dev.language === 'C') {
-      c++;
-    }
-    if (dev.language === 'JavaScript') {
-      javaScript++;
-    }
-    if (dev.language === 'Ruby') {
-      ruby++;
-    }
-
-    return {
-      C: c,
-      JavaScript: javaScript,
-      Ruby: ruby,
-    };
-  });
-
-  const countC = Object.values(languageCount)
-    .map((el) => el.C)
-    .reduce((accum, curr) => accum + curr, 0);
-  const countJavaScript = Object.values(languageCount)
-    .map((el) => el.JavaScript)
-    .reduce((accum, curr) => accum + curr, 0);
-  const countRuby = Object.values(languageCount)
-    .map((el) => el.Ruby)
-    .reduce((accum, curr) => accum + curr, 0);
-
-  return {
-    C: countC,
-    JavaScript: countJavaScript,
-    Ruby: countRuby,
-  };
+  return list
+    .map((el) => el['language'])
+    .reduce((accum, curr) => (accum[curr] = accum[curr] + 1 || 1), {});
 }
 
 countLanguages(list1); //?
