d1812db83284ea60f1e2a1ccdf4c7b2172ad311b
Paul Tobin <tobster111@gmail.com>
2021-05-31 20:41:45 +0100

    indexOf

--- JavaScript/FreeCodeCamp/basic-data-structure.js
+++ JavaScript/FreeCodeCamp/basic-data-structure.js
@@ -11,7 +11,7 @@ function copyMachine(arr, num) {
   let newArr = [];
   while (num >= 1) {
     // Only change code below this line
-    newArr.push([...arr]); 
+    newArr.push([...arr]);
     // Only change code above this line
     num--;
   }
@@ -23,8 +23,34 @@ copyMachine([true, false, true], 2); //?
 // Combine Arrays with the Spread Operator
 function spreadOut() {
   let fragment = ['to', 'code'];
-  let sentence = ["learning", ...fragment, "is", "fun"]
+  let sentence = ['learning', ...fragment, 'is', 'fun'];
   return sentence;
 }
 
-spreadOut() //?
+spreadOut(); //?
+
+// Iterate Through All an Array's Items Using For LoopsPassed
+function filteredArray(arr, elem) {
+  let newArr = [];
+  // change code below this line
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].indexOf(elem) == -1) {
+      //Checks every parameter for the element and if is NOT there continues the code
+      newArr.push(arr[i]); //Inserts the element of the array in the new filtered array
+    }
+  }
+
+  // change code above this line
+  return newArr;
+}
+
+filteredArray(
+  [
+    [3, 2, 3],
+    [1, 6, 3],
+    [3, 13, 26],
+    [19, 3, 9],
+  ],
+  3
+); //?
