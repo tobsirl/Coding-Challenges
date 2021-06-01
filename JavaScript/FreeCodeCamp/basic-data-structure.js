5cd7ac765172e3889f4887b95e4a7ff3eb467e1d
Paul Tobin <tobster111@gmail.com>
2021-06-01 20:24:08 +0100

    Assign values to a nested object

--- JavaScript/FreeCodeCamp/basic-data-structure.js
+++ JavaScript/FreeCodeCamp/basic-data-structure.js
@@ -84,3 +84,19 @@ foods.grapes = 35;
 const strawberries = 'strawberries';
 foods[strawberries] = 27;
 foods;
+
+// Modify an Object Nested Within an Object
+// Here we've defined an object userActivity, which includes another
+// object nested within it. Set the value of the online key to 45.
+
+let userActivity = {
+  id: 23894201352,
+  date: 'January 1, 2017',
+  data: {
+    totalUsers: 51,
+    online: 42,
+  },
+};
+
+userActivity.data.online = 45;
+userActivity
