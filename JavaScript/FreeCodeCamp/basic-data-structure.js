c34e02be4ba32f6dcfe86f2af6a90a89760fd860
Paul Tobin <tobster111@gmail.com>
2021-06-01 20:14:38 +0100

    Assign values to objects

--- JavaScript/FreeCodeCamp/basic-data-structure.js
+++ JavaScript/FreeCodeCamp/basic-data-structure.js
@@ -65,3 +65,22 @@ let myNestedArray = [
   ['iterate', 1.3849, 7, 'deepest', 'arbitrary', 'depth'],
   // Only change code above this line
 ];
+
+// Add Key-Value Pairs to JavaScript ObjectsPassed
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+};
+
+/*
+A foods object has been created with three entries. 
+Using the syntax of your choice, add three more entries to it: 
+bananas with a value of 13, grapes with a value of 35, and 
+strawberries with a value of 27.
+*/
+foods['bananas'] = 13;
+foods.grapes = 35;
+const strawberries = 'strawberries';
+foods[strawberries] = 27;
+foods;
