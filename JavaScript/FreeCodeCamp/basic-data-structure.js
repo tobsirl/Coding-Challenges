c4307a165c5456528f270ced30a40c332e61d3ba
Paul Tobin <tobster111@gmail.com>
2021-06-01 20:33:04 +0100

    Objects

--- JavaScript/FreeCodeCamp/basic-data-structure.js
+++ JavaScript/FreeCodeCamp/basic-data-structure.js
@@ -99,4 +99,27 @@ let userActivity = {
 };
 
 userActivity.data.online = 45;
-userActivity
+userActivity;
+
+// Access Property Names with Bracket Notation
+/*
+We've defined a function, checkInventory, which receives a scanned item as an argument. 
+Return the current value of the scannedItem key in the foods object. 
+You can assume that only valid keys will be provided as an argument to checkInventory.
+*/
+let products = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27,
+};
+
+function checkInventory(scannedItem) {
+  // Only change code below this line
+  return products[scannedItem];
+  // Only change code above this line
+}
+
+checkInventory('apples'); //?
