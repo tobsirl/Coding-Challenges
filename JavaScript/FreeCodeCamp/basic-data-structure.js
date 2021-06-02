910ab75aadb3e84f6b4436ac9b1cd99237f83853
Paul Tobin <tobster111@gmail.com>
2021-06-02 20:09:22 +0100

    delete

--- JavaScript/FreeCodeCamp/basic-data-structure.js
+++ JavaScript/FreeCodeCamp/basic-data-structure.js
@@ -123,3 +123,23 @@ function checkInventory(scannedItem) {
 }
 
 checkInventory('apples'); //?
+
+// Use the delete Keyword to Remove Object Properties
+// Use the delete keyword to remove the oranges, plums, and strawberries keys from the foods object.
+let produces = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27,
+};
+
+// Only change code below this line
+delete produces.oranges;
+delete produces.plums;
+delete produces.strawberries;
+
+// Only change code above this line
+
+produces; 
