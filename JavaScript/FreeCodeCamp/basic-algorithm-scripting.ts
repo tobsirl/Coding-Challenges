668014006bf2bf60bad508a5905aaa800dfae0c0
Paul Tobin <tobster111@gmail.com>
2021-06-04 18:25:33 +0100

    Largest Numbers in Arrays

--- JavaScript/FreeCodeCamp/basic-algorithm-scripting.ts
+++ JavaScript/FreeCodeCamp/basic-algorithm-scripting.ts
@@ -32,3 +32,25 @@ function findLongestWordLength(str: string): number {
 }
 
 findLongestWordLength('The quick brown fox jumped over the lazy dog');
+
+// Return Largest Numbers in Arrays
+function largestOfFour(arr: Array<number>): Array<number> {
+  let results = [];
+  for (let i = 0; i < arr.length; i++) {
+    let maxNum = arr[i][0];
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] > maxNum) {
+        maxNum = arr[i][j];
+      }
+    }
+    results[i] = maxNum;
+  }
+  return results;
+}
+
+largestOfFour([
+  [4, 5, 1, 3],
+  [13, 27, 18, 26],
+  [32, 35, 37, 39],
+  [1000, 1001, 857, 1],
+]);
