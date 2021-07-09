ed8940b8a2db431f843d95aade2cdb2f45c3307a
Paul Tobin <tobster111@gmail.com>
2021-07-09 18:21:19 +0100

    Understand Functional Programming Terminology

--- JavaScript/FreeCodeCamp/functional-programming.js
+++ JavaScript/FreeCodeCamp/functional-programming.js
@@ -1,13 +1,39 @@
 // Learn About Functional Programming
 // Function that returns a string representing a cup of green tea
-const prepareTea = () => 'greenTea';
+// const prepareTea = () => 'greenTea';
 
 /*
 Given a function (representing the tea type) and number of cups needed, the
 following function returns an array of strings (each representing a cup of
 a specific type of tea).
 */
-const getTea = (numOfCups) => {
+// const getTea = (numOfCups) => {
+//   const teaCups = [];
+
+//   for (let cups = 1; cups <= numOfCups; cups += 1) {
+//     const teaCup = prepareTea();
+//     teaCups.push(teaCup);
+//   }
+//   return teaCups;
+// };
+
+// Only change code below this line
+// const tea4TeamFCC = getTea(40);
+// Only change code above this line
+
+// Understand Functional Programming Terminology
+// Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (prepareTea, numOfCups) => {
   const teaCups = [];
 
   for (let cups = 1; cups <= numOfCups; cups += 1) {
@@ -18,5 +44,8 @@ const getTea = (numOfCups) => {
 };
 
 // Only change code below this line
-const tea4TeamFCC = getTea(40);
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
 // Only change code above this line
+
+console.log(tea4GreenTeamFCC, tea4BlackTeamFCC);
