6f84f044c00b507a809dee7758a0335072d6a683
Paul Tobin <tobster111@gmail.com>
2021-07-13 18:57:15 +0100

    Refactor Global Variables Out of FunctionsPassed

--- JavaScript/FreeCodeCamp/functional-programming.js
+++ JavaScript/FreeCodeCamp/functional-programming.js
@@ -125,3 +125,32 @@ function incrementer(value) {
 }
 
 const defferentValue = incrementer(fixedValue);
+
+// Refactor Global Variables Out of FunctionsPassed
+// The global variable
+var bookList = [
+  'The Hound of the Baskervilles',
+  'On The Electrodynamics of Moving Bodies',
+  'Philosophiæ Naturalis Principia Mathematica',
+  'Disquisitiones Arithmeticae',
+];
+
+// Change code below this line
+function add(list, bookName) {
+  return [...list, bookName];
+  // Change code above this line
+}
+
+// Change code below this line
+function remove(list, bookName) {
+  return list.filter((book) => book !== bookName);
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(
+  add(bookList, 'A Brief History of Time'),
+  'On The Electrodynamics of Moving Bodies'
+);
+
+bookList;
