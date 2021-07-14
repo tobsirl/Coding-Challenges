7340f164b1407a781750cf56278bc43f57b726df
Paul Tobin <tobster111@gmail.com>
2021-07-14 18:56:10 +0100

    Solution

--- JavaScript/FreeCodeCamp/functional-programming.js
+++ JavaScript/FreeCodeCamp/functional-programming.js
@@ -451,13 +451,20 @@ console.log(filteredList);
 // The global variable
 var s = [23, 65, 98, 5];
 
-Array.prototype.myFilter = function(callback) {
+Array.prototype.myFilter = function (callback) {
   // Only change code below this line
   var newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i]) === true) {
+      newArray.push(this[i]);
+    }
+  }
   // Only change code above this line
   return newArray;
 };
 
-var new_s = s.myFilter(function(item) {
+var new_s = s.myFilter(function (item) {
   return item % 2 === 1;
 });
+
+new_s;
