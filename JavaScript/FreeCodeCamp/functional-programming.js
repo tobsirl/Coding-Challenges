8ea07bb8b57f2e159778466d3f3d79048e41aeee
Paul Tobin <tobster111@gmail.com>
2021-07-15 18:27:31 +0100

    Complete

--- JavaScript/FreeCodeCamp/functional-programming.js
+++ JavaScript/FreeCodeCamp/functional-programming.js
@@ -481,9 +481,9 @@ sliceArray(inputAnim, 1, 3); //?
 // Remove Elements from an Array Using slice Instead of splice
 function nonMutatingSplice(cities) {
   // Only change code below this line
-  return cities.splice(3);
+  return cities.slice(0, 3);
 
   // Only change code above this line
 }
-var inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
-nonMutatingSplice(inputCities);
+var inputCities = ['Chicago', 'Delhi', 'Islamabad', 'London', 'Berlin'];
+nonMutatingSplice(inputCities); //?
