7271ff0f7bd809f9be1ebbf2082af52dacd5cdb7
Paul Tobin <tobster111@gmail.com>
2021-07-15 18:33:48 +0100

    Concat 2

--- JavaScript/FreeCodeCamp/functional-programming.js
+++ JavaScript/FreeCodeCamp/functional-programming.js
@@ -497,3 +497,14 @@ function nonMutatingConcat(original, attach) {
 var first = [1, 2, 3];
 var second = [4, 5];
 nonMutatingConcat(first, second);
+
+// Add Elements to the End of an Array Using concat Instead of push
+function nonMutatingPush(original, newItem) {
+  // Only change code below this line
+  return original.concat(newItem);
+
+  // Only change code above this line
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+nonMutatingPush(first, second);
