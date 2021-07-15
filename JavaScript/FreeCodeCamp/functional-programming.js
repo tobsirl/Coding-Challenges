30c8c46b85a46c6f55da3b2c980f14b8d63cec4a
Paul Tobin <tobster111@gmail.com>
2021-07-15 18:23:13 +0100

    Complete

--- JavaScript/FreeCodeCamp/functional-programming.js
+++ JavaScript/FreeCodeCamp/functional-programming.js
@@ -468,3 +468,12 @@ var new_s = s.myFilter(function (item) {
 });
 
 new_s;
+
+// Return Part of an Array Using the slice Method
+function sliceArray(anim, beginSlice, endSlice) {
+  // Only change code below this line
+  return anim.slice(beginSlice, endSlice);
+  // Only change code above this line
+}
+var inputAnim = ['Cat', 'Dog', 'Tiger', 'Zebra', 'Ant'];
+sliceArray(inputAnim, 1, 3); //?
