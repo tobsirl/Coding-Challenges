9b30c1cda2113105b539ad52714b4e9cca02f957
Paul Tobin <tobster111@gmail.com>
2021-07-28 19:48:43 +0100

    Setup for challenge

--- JavaScript/FreeCodeCamp/functional-programming.js
+++ JavaScript/FreeCodeCamp/functional-programming.js
@@ -647,3 +647,13 @@ function getRating(watchList) {
 }
 
 getRating(watchList); //?
+
+// Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem
+const squareList = (arr) => {
+  // Only change code below this line
+  return arr;
+  // Only change code above this line
+};
+
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+console.log(squaredIntegers);
