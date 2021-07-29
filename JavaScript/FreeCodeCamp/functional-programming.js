1e74ff9ac61ba008674eb628da05e8f021234678
Paul Tobin <tobster111@gmail.com>
2021-07-29 18:30:16 +0100

    Setup and solution

--- JavaScript/FreeCodeCamp/functional-programming.js
+++ JavaScript/FreeCodeCamp/functional-programming.js
@@ -678,3 +678,13 @@ function alphabeticalOrder(arr) {
 }
 
 alphabeticalOrder(['a', 'd', 'c', 'a', 'z', 'g']);
+
+// Return a Sorted Array Without Changing the Original Array
+var globalArray = [5, 6, 3, 2, 9];
+function nonMutatingSort(arr) {
+  // Only change code below this line
+  const copyArr = [...arr];
+  return copyArr.sort((a, b) => a - b);
+  // Only change code above this line
+}
+nonMutatingSort(globalArray);
