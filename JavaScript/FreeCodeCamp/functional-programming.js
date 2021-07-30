b5b4dea779f5abd15d903a504d59ea9d4c1a1ef5
Paul Tobin <tobster111@gmail.com>
2021-07-30 18:49:40 +0100

    Solution

--- JavaScript/FreeCodeCamp/functional-programming.js
+++ JavaScript/FreeCodeCamp/functional-programming.js
@@ -692,8 +692,7 @@ nonMutatingSort(globalArray);
 // Split a String into an Array Using the split Method
 function splitify(str) {
   // Only change code below this line
-
-
+  return str.split(/\W/);
   // Only change code above this line
 }
-splitify("Hello World,I-am code");
+splitify('Hello World,I-am code');
