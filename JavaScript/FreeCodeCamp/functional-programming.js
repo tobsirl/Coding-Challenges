76c1051d58f3f1349f60a759505d4b9a285ce95d
Paul Tobin <tobster111@gmail.com>
2021-07-30 18:53:26 +0100

    Solution

--- JavaScript/FreeCodeCamp/functional-programming.js
+++ JavaScript/FreeCodeCamp/functional-programming.js
@@ -700,8 +700,8 @@ splitify('Hello World,I-am code');
 // Combine an Array into a String Using the join Method
 function sentensify(str) {
   // Only change code below this line
-
+  return str.split(/[-,.]/).join(' ');
 
   // Only change code above this line
 }
-sentensify("May-the-force-be-with-you");
+sentensify('May-the-force-be-with-you');
