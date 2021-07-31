7b2894069abb61d7db89a09dc6470dddc9d2d576
Paul Tobin <tobster111@gmail.com>
2021-07-31 17:56:34 +0100

    Solution

--- JavaScript/FreeCodeCamp/functional-programming.js
+++ JavaScript/FreeCodeCamp/functional-programming.js
@@ -722,7 +722,9 @@ urlSlug('Hold The Door');
 function checkPositive(arr) {
   // Only change code below this line
 
-
+  return arr.every((number) => number > 0);
   // Only change code above this line
 }
 checkPositive([1, 2, 3, -4, 5]);
+checkPositive([1, 2, 3, 4, 5]);
+checkPositive([1, -2, 3, -4, 5]);
