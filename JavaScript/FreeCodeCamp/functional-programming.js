f32eea79e4b74a4aca2b04145061a878da1676f6
Paul Tobin <tobster111@gmail.com>
2021-08-01 11:52:35 +0100

    Solution

--- JavaScript/FreeCodeCamp/functional-programming.js
+++ JavaScript/FreeCodeCamp/functional-programming.js
@@ -732,8 +732,7 @@ checkPositive([1, -2, 3, -4, 5]);
 // Use the some Method to Check that Any Elements in an Array Meet a Criteria
 function checkPositive(arr) {
   // Only change code below this line
-
-
+  return arr.some((number) => number > 0);
   // Only change code above this line
 }
 checkPositive([1, 2, 3, -4, 5]);
