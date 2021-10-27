9faceef1f665ea06ede3a8702e5fd005148674b1
Paul Tobin <tobster111@gmail.com>
2021-10-27 18:51:24 +0100

    function getSuspects

--- JavaScript/Frontend-Masters-JS/list-transformations.js
+++ JavaScript/Frontend-Masters-JS/list-transformations.js
@@ -6,3 +6,20 @@ const game = {
 };
 
 game['suspects'];
+
+const suspects = game.suspects.map(({ name, color }) => {
+  return {
+    personsName: name,
+    color,
+  };
+});
+
+function getSuspects() {
+  let result = [];
+  for (let i = 0; i < game.suspects.length; i++) {
+    result.push(game.suspects[i]);
+  }
+  return result;
+}
+
+getSuspects(); //?
