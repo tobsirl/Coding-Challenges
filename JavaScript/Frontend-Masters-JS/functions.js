778e09dcf3c6aa5408f166f6cb1df0ee7512a988
Paul Tobin <tobster111@gmail.com>
2021-10-28 18:36:29 +0100

    Setup of functions example

---
+++ JavaScript/Frontend-Masters-JS/functions.js
@@ -0,0 +1,13 @@
+function CreateSuspectObjects(name) {
+  return {
+    name: name,
+    color: name.split(' ')[1],
+    speak() {
+      console.log(`My name is ${name}`);
+    },
+  };
+}
+
+const suspects = ['Miss Scarlet', 'Colonel Mustard', 'Mr. White'];
+
+const suspectList = [];
