5a9ce55b8d1f9a10bdc58267168a7f3f1924c61a
Paul Tobin <tobster111@gmail.com>
2021-10-28 19:19:33 +0100

    Implementing _.each

--- JavaScript/Frontend-Masters-JS/functions.js
+++ JavaScript/Frontend-Masters-JS/functions.js
@@ -15,3 +15,27 @@ const suspectList = [];
 suspects.map((name) => suspectList.push(CreateSuspectObjects(name)));
 
 suspectList; //?
+
+const _ = {};
+
+_.each = function (list, callback) {
+  if (Array.isArray(list)) {
+    for (let i = 0; i < list.length; i++) {
+      callback(list[i], i, list);
+    }
+  } else {
+    for (const key in list) {
+      if (Object.hasOwnProperty.call(list, key)) {
+        const element = list[key];
+      }
+    }
+  }
+};
+
+_.each(['sally', 'georgie', 'porgie'], function (name, i, list) {
+  if (list[i + 1]) {
+    console.log(name, 'is younger than ', list[i + 1]);
+  } else {
+    console.log(name, 'is the oldest');
+  }
+});
