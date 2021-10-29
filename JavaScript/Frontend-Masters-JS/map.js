b5bf7e79e9f53930a4f5d3c566d5416c0542a870
Paul Tobin <tobster111@gmail.com>
2021-10-29 19:55:26 +0100

    Map 2

--- JavaScript/Frontend-Masters-JS/map.js
+++ JavaScript/Frontend-Masters-JS/map.js
@@ -3,3 +3,15 @@ const weapons = ['candlestick', 'lead pipe', 'revolver'];
 const makeBroken = function (item) {
   return `broken ${item}`;
 };
+
+const _ = {};
+
+_.map = function (weapons, callback) {
+  let list = [];
+  for (let i = 0; i < weapons.length; i++) {
+    list.push(callback(weapons[i]));
+  }
+  return list;
+};
+
+_.map(weapons, makeBroken); //?
