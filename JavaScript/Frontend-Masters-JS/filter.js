fc69b507ec049d1a33ee4f1032342362b1597d20
Paul Tobin <tobster111@gmail.com>
2021-10-30 11:21:37 +0100

    Implementing filter

--- JavaScript/Frontend-Masters-JS/filter.js
+++ JavaScript/Frontend-Masters-JS/filter.js
@@ -1,75 +1,86 @@
 const videoData = [
   {
-      name: 'Miss Scarlet',
-      present: true,
-      rooms: [
-          {kitchen: false},
-          {ballroom: false},
-          {conservatory: false},
-          {'dining room': false},
-          {'billiard room': false},
-          {library: false}
-      ]
+    name: 'Miss Scarlet',
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { 'dining room': false },
+      { 'billiard room': false },
+      { library: false },
+    ],
   },
   {
-      name: 'Mrs. White',
-      present: false,
-      rooms: [
-          {kitchen: false},
-          {ballroom: false},
-          {conservatory: false},
-          {'dining room': false},
-          {'billiard room': false},
-          {library: false}
-      ]
+    name: 'Mrs. White',
+    present: false,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { 'dining room': false },
+      { 'billiard room': false },
+      { library: false },
+    ],
   },
   {
-      name: 'Reverend Green',
-      present: true,
-      rooms: [
-          {kitchen: false},
-          {ballroom: false},
-          {conservatory: false},
-          {'dining room': false},
-          {'billiard room': false},
-          {library: false}
-      ]
+    name: 'Reverend Green',
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { 'dining room': false },
+      { 'billiard room': false },
+      { library: false },
+    ],
   },
   {
-      name: 'Rusty',
-      present: false,
-      rooms: [
-          {kitchen: false},
-          {ballroom: false},
-          {conservatory: false},
-          {'dining room': false},
-          {'billiard room': false},
-          {library: false}
-      ]
+    name: 'Rusty',
+    present: false,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { 'dining room': false },
+      { 'billiard room': false },
+      { library: false },
+    ],
   },
   {
-      name: 'Colonel Mustard',
-      present: true,
-      rooms: [
-          {kitchen: false},
-          {ballroom: false},
-          {conservatory: false},
-          {'dining room': false},
-          {'billiard room': false},
-          {library: false}
-      ]
+    name: 'Colonel Mustard',
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { 'dining room': false },
+      { 'billiard room': false },
+      { library: false },
+    ],
   },
   {
-      name: 'Professor Plum',
-      present: true,
-      rooms: [
-          {kitchen: false},
-          {ballroom: false},
-          {conservatory: false},
-          {'dining room': false},
-          {'billiard room': false},
-          {library: false}
-      ]
-  }
+    name: 'Professor Plum',
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { 'dining room': false },
+      { 'billiard room': false },
+      { library: false },
+    ],
+  },
 ];
 
+const _ = {};
+
+_.filter = function (arr, callback) {
+  let storage = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i], i, arr) === true) {
+      storage.push(arr[i]);
+    }
+  }
+  return storage;
+};
