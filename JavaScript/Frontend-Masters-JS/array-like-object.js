5905f57f3993a00f15d789e4777ff49cee7f47b8
Paul Tobin <tobster111@gmail.com>
2021-10-31 10:13:01 +0000

    Using Array.from

--- JavaScript/Frontend-Masters-JS/array-like-object.js
+++ JavaScript/Frontend-Masters-JS/array-like-object.js
@@ -1,7 +1,16 @@
 const constructArr = function () {
-  const arr = Array.prototype.slice.call(arguments); 
+  const arr = Array.prototype.slice.call(arguments);
   arr.push('the billiards room?');
   return arr.join(' ');
 };
 
+constructArr('was', 'it', 'in');
+
+
+// using es6 Array.from
+const constructArr = function () {
+  const arr = Array.from(arguments);
+  arr.push('the billiards room?');
+  return arr.join(' ');
+};
 constructArr('was', 'it', 'in');
