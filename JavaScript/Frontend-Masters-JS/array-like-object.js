10819a19e375802eda93c8c419d9d20e25d8b8d9
Paul Tobin <tobster111@gmail.com>
2021-10-31 10:20:38 +0000

    Getting from

--- JavaScript/Frontend-Masters-JS/array-like-object.js
+++ JavaScript/Frontend-Masters-JS/array-like-object.js
@@ -6,11 +6,11 @@ const constructArr = function () {
 
 constructArr('was', 'it', 'in');
 
-
 // using es6 Array.from
 const constructArr = function () {
   const arr = Array.from(arguments);
   arr.push('the billiards room?');
   return arr.join(' ');
 };
+
 constructArr('was', 'it', 'in');
