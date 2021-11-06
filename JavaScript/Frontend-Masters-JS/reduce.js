637c7107ba484d9f75ab3467ca49bb013d83b001
Paul Tobin <tobster111@gmail.com>
2021-11-06 14:12:18 +0000

    First implementation

--- JavaScript/Frontend-Masters-JS/reduce.js
+++ JavaScript/Frontend-Masters-JS/reduce.js
@@ -1,6 +1,12 @@
-const reduce = function (list, cb, initial) {
+const reduce = function (list, cb, initial = list[0]) {
   // loop through list
+  let memo = initial;
+  for (let i = 0; i < list.length; i++) {
+    memo = cb(list[i], memo);
+
     // call the cb with arr[i], prev/initial
     // save the return value
+  }
   // return result
+  return memo;
 };
