06e9d41a50acafd959434f0b304d169f2833d4da
Paul Tobin <tobster111@gmail.com>
2021-11-06 11:27:09 +0000

    Using the rest operator to pass arguments

--- JavaScript/Frontend-Masters-JS/higher-order-functions.js
+++ JavaScript/Frontend-Masters-JS/higher-order-functions.js
@@ -58,3 +58,10 @@ const ifElse = (condition, isTrue, isFalse, p) => {
   return condition ? isTrue(p) : isFalse(p);
 };
 ifElse(true, fn1, fn2, 'HI');
+
+const ifElse = (condition, isTrue, isFalse, ...args) => {
+  console.log(args); //['HI', 'BYE', 'HOLA']
+  return condition ? isTrue(...args) : isFalse(...args);
+  isTrue('HI', 'BYE', 'HOLA');
+};
+ifElse(true, fn1, fn2, 'HI', 'BYE', 'HOLA');
