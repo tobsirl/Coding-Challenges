21003ffe5f4ce570ffa829ca26a1193cf76c8cf9
Paul Tobin <tobster111@gmail.com>
2021-11-06 19:42:19 +0000

    Part 1

--- JavaScript/Frontend-Masters-JS/emptyroom.js
+++ JavaScript/Frontend-Masters-JS/emptyroom.js
@@ -1,49 +1,54 @@
 const newDevelopment = [
   {
-      name: 'Miss Scarlet',
-      present: true,
-      rooms: [
-          {kitchen: false},
-          {ballroom: false},
-          {conservatory: true},
-          {'dining room': true},
-          {'billiard room': false},
-          {library: true}
-      ]
+    name: 'Miss Scarlet',
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: true },
+      { 'dining room': true },
+      { 'billiard room': false },
+      { library: true },
+    ],
   },
   {
-      name: 'Reverend Green',
-      present: true,
-      rooms: [
-          {kitchen: true},
-          {ballroom: false},
-          {conservatory: false},
-          {'dining room': false},
-          {'billiard room': true},
-          {library: false}
-      ]
+    name: 'Reverend Green',
+    present: true,
+    rooms: [
+      { kitchen: true },
+      { ballroom: false },
+      { conservatory: false },
+      { 'dining room': false },
+      { 'billiard room': true },
+      { library: false },
+    ],
   },
   {
-      name: 'Colonel Mustard',
-      present: true,
-      rooms: [
-          {kitchen: false},
-          {ballroom: false},
-          {conservatory: true},
-          {'dining room': false},
-          {'billiard room': true},
-          {library: false}
-      ]
+    name: 'Colonel Mustard',
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: true },
+      { 'dining room': false },
+      { 'billiard room': true },
+      { library: false },
+    ],
   },
   {
-      name: 'Professor Plum',
-      present: true,
-      rooms: [
-          {kitchen: true},
-          {ballroom: false},
-          {conservatory: false},
-          {'dining room': true},
-          {'billiard room': false},
-          {library: false}
-      ]
-  }
+    name: 'Professor Plum',
+    present: true,
+    rooms: [
+      { kitchen: true },
+      { ballroom: false },
+      { conservatory: false },
+      { 'dining room': true },
+      { 'billiard room': false },
+      { library: false },
+    ],
+  },
+];
+
+const noRoom = newDevelopment.reduce((accum, curr) => {
+  
+}, {})
