7f8bc0eaf8902899cbc8a819b40cbc75beb86e94
Paul Tobin <tobster111@gmail.com>
2021-11-06 11:20:16 +0000

    Passing agruments to higher order components

--- JavaScript/Frontend-Masters-JS/higher-order-functions.js
+++ JavaScript/Frontend-Masters-JS/higher-order-functions.js
@@ -28,18 +28,33 @@ ifElse(
   }
 ); //?
 
-var increment = function (n) {
-  return n + 1;
-};
+// var increment = function (n) {
+//   return n + 1;
+// };
 
-var square = function (n) {
-  return n * n;
-};
+// var square = function (n) {
+//   return n * n;
+// };
 
-var doMathSoIDontHaveTo = function (n, func) {
-  return func(n);
-};
+// var doMathSoIDontHaveTo = function (n, func) {
+//   return func(n);
+// };
+
+// doMathSoIDontHaveTo(5, square);
+
+// doMathSoIDontHaveTo(4, increment);
+
+// ES6
+const increment = (n) => n + 1;
+const square = (n) => n * n;
+
+const doMathSoIDontHaveTo = (n, func) => func(n);
 
 doMathSoIDontHaveTo(5, square);
-
 doMathSoIDontHaveTo(4, increment);
+
+//How do we pass arguments?
+const ifElse = (condition, isTrue, isFalse, p) => {
+  return condition ? isTrue(p) : isFalse(p);
+};
+ifElse(true, fn1, fn2, 'HI');
