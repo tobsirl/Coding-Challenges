f728aad0d8e2bf78ecc0752382b66013758f39b1
Paul Tobin <tobster111@gmail.com>
2021-11-07 12:41:22 +0000

    Curring solution

--- JavaScript/Frontend-Masters-JS/currying.js
+++ JavaScript/Frontend-Masters-JS/currying.js
@@ -1,15 +1,26 @@
 // currying
-_.curry(func, [arity=func.length])
+// _.curry(func, [(arity = func.length)]);
 
- 
-var abc = function(a, b, c) {
+var abc = function (a, b, c) {
   return [a, b, c];
 };
- 
-var curried = _.curry(abc);
- 
-curried(1)(2)(3);
+
+// var curried = _.curry(abc);
+
+// curried(1)(2)(3);
 // => [1, 2, 3]
- 
-curried(1, 2)(3);
+
+// curried(1, 2)(3);
 // => [1, 2, 3]
+
+// solution
+const curry = (fn) => {
+  return (arg) => {
+    return (arg2) => {
+      return fn(arg, arg2);
+    };
+  };
+};
+
+const curriedTest = curry(abc);
+curriedTest(1)(2); //?
