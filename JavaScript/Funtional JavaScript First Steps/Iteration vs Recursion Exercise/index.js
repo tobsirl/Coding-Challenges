89befad1972a6444f855ddf6dc663f7d06ab3eb6
Paul Tobin <tobster111@gmail.com>
2022-04-02 11:50:54 +0100

    Solution

---
+++ JavaScript/Funtional JavaScript First Steps/Iteration vs Recursion Exercise/index.js
@@ -0,0 +1,28 @@
+function iterativeFibonacci(n) {
+  // TODO your code goes here
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  let previous = 0;
+  let current = 1;
+  for (let i = n; i > 1; i--) {
+    let next = previous + current;
+    previous = current;
+    current = next;
+  }
+  return current;
+}
+
+iterativeFibonacci(10); //?
+
+function recursiveFibonacci(n) {
+  // TODO your code goes here
+  // base case
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  // recursive case
+  return recursiveFibonacci(n - 2) + recursiveFibonacci(n - 1);
+}
+
+recursiveFibonacci(10); //?
