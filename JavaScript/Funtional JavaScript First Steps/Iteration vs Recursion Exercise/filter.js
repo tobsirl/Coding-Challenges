930eb92d8510382d6f0e4cb757b36c6014539591
Paul Tobin <tobster111@gmail.com>
2022-04-03 10:13:29 +0100

    Evens

--- JavaScript/Funtional JavaScript First Steps/Iteration vs Recursion Exercise/filter.js
+++ JavaScript/Funtional JavaScript First Steps/Iteration vs Recursion Exercise/filter.js
@@ -17,3 +17,19 @@ function head(array) {
 function tail(array) {
   return array.slice(1);
 }
+
+function filter(predicateFn, array) {
+  if (length(array) === 0) return [];
+  const firstItem = head(array);
+  const filteredFirst = predicateFn(firstItem) ? [firstItem] : [];
+  return concat(filteredFirst, filter(predicateFn, tail(array)));
+}
+
+const wholes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function isEven(n) {
+  // TODO your code goes here
+  return n % 2 === 0;
+}
+
+const evens = filter(isEven, wholes);
