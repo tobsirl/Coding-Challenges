e59118ed127b9bd164669d1a56208945c987ab5f
Paul Tobin <tobster111@gmail.com>
2022-04-03 10:43:41 +0100

    isPrime

--- JavaScript/Funtional JavaScript First Steps/Iteration vs Recursion Exercise/filter.js
+++ JavaScript/Funtional JavaScript First Steps/Iteration vs Recursion Exercise/filter.js
@@ -46,3 +46,14 @@ const greaterThanFour = filter(
   },
   wholes
 );
+
+function isPrime(n) {
+  // TODO your code goes here
+  if (n <= 1) return false;
+  const wholes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const possibleFactors = filter((m) => m > 1 && m < n, wholes);
+  const factors = filter((m) => n % m === 0, possibleFactors);
+  return factors.length === 0 ? true : false;
+}
+
+const primes = filter(isPrime, wholes); 
