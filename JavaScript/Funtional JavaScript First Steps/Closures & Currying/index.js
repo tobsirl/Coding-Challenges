aab34f296076f1f5b8e9f5ac7b8626d41b2eff90
Paul Tobin <tobster111@gmail.com>
2022-04-05 18:20:33 +0100

    Closures

--- JavaScript/Funtional JavaScript First Steps/Closures & Currying/index.js
+++ JavaScript/Funtional JavaScript First Steps/Closures & Currying/index.js
@@ -7,3 +7,15 @@ function signMessageWith(salutation) {
 const writeBusinessMemo = signMessageWith('Best');
 
 writeBusinessMemo('Hi, Peter. We need to talk about your TPS reports.', 'Bob'); //?
+
+function makeSignature(salutation, name) {
+  return (body) => {
+    return `${salutation} ${body} ${name}`;
+  };
+}
+
+const writeLoveLetter = makeSignature('Thine evermore', 'William');
+
+writeLoveLetter(
+  "Shall I compare thee to a summer's day? Thou art more lovely and more temperate."
+); //?
