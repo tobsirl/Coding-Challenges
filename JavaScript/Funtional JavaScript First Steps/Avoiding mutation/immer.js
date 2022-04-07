a9d1a2e5e38687a72bd75b947b3e69901d44019a
Paul Tobin <tobster111@gmail.com>
2022-04-07 18:18:00 +0100

    produce

--- JavaScript/Funtional JavaScript First Steps/Avoiding mutation/immer.js
+++ JavaScript/Funtional JavaScript First Steps/Avoiding mutation/immer.js
@@ -3,3 +3,13 @@ const immer = require('immer');
 immer.produce;
 
 const regularJSArray = [1, 2, 3];
+
+const newArray = immer.produce(regularJSArray, (draft) => {
+  draft.push(4);
+});
+
+regularJSArray.length; 
+
+newerArray = immer.produce(newArray, (draft) => {
+  draft[0] = 5;
+}); 
