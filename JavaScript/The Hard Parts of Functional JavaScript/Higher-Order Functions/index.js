5991a833629e21ec92a14bf26de4f9de54aafa13
Paul Tobin <tobster111@gmail.com>
2022-04-10 10:27:43 +0100

    Challenge 2

--- JavaScript/The Hard Parts of Functional JavaScript/Higher-Order Functions/index.js
+++ JavaScript/The Hard Parts of Functional JavaScript/Higher-Order Functions/index.js
@@ -11,3 +11,14 @@ const addTwo = (num) => {
 };
 console.log(addTwo(3));
 console.log(addTwo(10));
+
+/**
+ *  Challenge 2
+ *  Create a function addS that accepts one input and adds an "s" to it.
+ */
+const addS = (word) => {
+  return `${word}s`;
+};
+
+console.log(addS('pizza'));
+console.log(addS('bagel'));
