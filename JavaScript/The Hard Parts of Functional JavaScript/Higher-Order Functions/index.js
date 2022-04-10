3f095ef662dcb86017395b3475e9503536243fc7
Paul Tobin <tobster111@gmail.com>
2022-04-10 10:21:49 +0100

    Challenge 1

--- JavaScript/The Hard Parts of Functional JavaScript/Higher-Order Functions/index.js
+++ JavaScript/The Hard Parts of Functional JavaScript/Higher-Order Functions/index.js
@@ -6,3 +6,8 @@
   Challenge 1
   Create a function addTwo that accepts one input and adds 2 to it. 
 */
+const addTwo = (num) => {
+  return num + 2;
+};
+console.log(addTwo(3));
+console.log(addTwo(10));
