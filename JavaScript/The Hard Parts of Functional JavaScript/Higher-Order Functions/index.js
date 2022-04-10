acaa228761bd24ba9e5812ea369e9937780591e4
Paul Tobin <tobster111@gmail.com>
2022-04-10 10:37:54 +0100

    Challenge 3

--- JavaScript/The Hard Parts of Functional JavaScript/Higher-Order Functions/index.js
+++ JavaScript/The Hard Parts of Functional JavaScript/Higher-Order Functions/index.js
@@ -22,3 +22,20 @@ const addS = (word) => {
 
 console.log(addS('pizza'));
 console.log(addS('bagel'));
+
+/**
+ * Challenge 3
+ * Create a function called map that takes two inputs:
+ * an array of numbers (a list of numbers)
+ * a 'callback' function - a function that is applied to each element of the array (inside of the function 'map')
+ * Have map return a new array filled with numbers that are the result of using the 'callback' function on each element of the input array.
+ */
+const map = (array, callback) => {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    result.push(callback(array[i]));
+  }
+  return result;
+};
+
+console.log(map([1, 2, 3], addTwo));
