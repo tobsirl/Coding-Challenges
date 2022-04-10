7fb1a706e362479e52ac3c326d35b21ee9fea570
Paul Tobin <tobster111@gmail.com>
2022-04-10 10:20:24 +0100

    Setup

---
+++ JavaScript/The Hard Parts of Functional JavaScript/Higher-Order Functions/index.js
@@ -0,0 +1,8 @@
+// ##########################
+// # Higher-Order Functions #
+// ##########################
+
+/* 
+  Challenge 1
+  Create a function addTwo that accepts one input and adds 2 to it. 
+*/
