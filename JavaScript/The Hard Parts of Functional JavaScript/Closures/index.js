c1f6154121d68aeba910754c01e90f0440480855
Paul Tobin <tobster111@gmail.com>
2022-04-14 15:49:17 +0100

    Challenge 3

--- JavaScript/The Hard Parts of Functional JavaScript/Closures/index.js
+++ JavaScript/The Hard Parts of Functional JavaScript/Closures/index.js
@@ -16,8 +16,8 @@ const createFunction = () => {
 const function1 = createFunction();
 function1();
 
-// Challenge 2
 /*
+ *  Challenge 2
  *  Create a function createFunctionPrinter that accepts one input and returns a function.
  *  When that created function is called, it should print out the input that was used when the function was created.
  *  const createFunctionPrinter = (input) => {};
@@ -34,3 +34,32 @@ const printSample = createFunctionPrinter('sample');
 printSample();
 const printHello = createFunctionPrinter('hello');
 printHello();
+
+/*
+ *  Challenge 3
+ *  Examine the code for the outer function.
+ *  Notice that we are returning a function and that function is using variables that are outside of its scope.
+ *  Uncomment those lines of code. Try to deduce the output before executing.
+ */
+
+const outer = () => {
+  let counter = 0; // this variable is outside incrementCounter's scope
+  const incrementCounter = () => {
+    counter++;
+    console.log('counter', counter);
+  };
+  return incrementCounter;
+};
+
+const willCounter = outer();
+const jasCounter = outer();
+
+// Uncomment each of these lines one by one.
+// Before your do, guess what will be logged from each function call.
+
+willCounter();
+willCounter();
+willCounter();
+
+jasCounter();
+willCounter();
