1781bea466544cf7074d0f208fb1c4ad328c6bec
Paul Tobin <tobster111@gmail.com>
2022-04-14 15:33:29 +0100

    Challenge 2

--- JavaScript/The Hard Parts of Functional JavaScript/Closures/index.js
+++ JavaScript/The Hard Parts of Functional JavaScript/Closures/index.js
@@ -15,3 +15,22 @@ const createFunction = () => {
 // UNCOMMENT THESE TO TEST YOUR WORK!
 const function1 = createFunction();
 function1();
+
+// Challenge 2
+/*
+ *  Create a function createFunctionPrinter that accepts one input and returns a function.
+ *  When that created function is called, it should print out the input that was used when the function was created.
+ *  const createFunctionPrinter = (input) => {};
+ */
+
+function createFunctionPrinter(params) {
+  return function () {
+    return params;
+  };
+}
+
+// UNCOMMENT THESE TO TEST YOUR WORK!
+const printSample = createFunctionPrinter('sample');
+printSample();
+const printHello = createFunctionPrinter('hello');
+printHello();
